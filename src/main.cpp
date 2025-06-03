46c5cffd9a23f92feb37a68851619dff43f96381
Luna <72428016+Lunas-Lab@users.noreply.github.com>
2025-06-03 14:02:58 +1200

    Changed error light to blinking rather than staying on

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,37 @@
 #include <Arduino.h>
 #include <PWM.h>
 
-static const uint8_t pwmPin = 9; //Pin that goes to fam PWM input (Arduino Uno have a built LED on pin 13)
-static const uint32_t frequency = 22000; //PWM frequency in Hz
-static const uint8_t errorPin = 13; // Pin to set HIGH if pin PWM frequency is not set successfully
+static const uint8_t pwmPin = 9;         // Pin that goes to fam PWM input (Arduino Uno have a built LED on pin 13)
+static const uint32_t frequency = 22000; // PWM frequency in Hz
+static const uint8_t errorPin = 13;      // Pin to blink if pin PWM frequency is not set successfully
 
-void setup() {
-  //Setting up error pin 
+void setup()
+{
+  // Setting up error pin
   pinMode(errorPin, OUTPUT);
   digitalWrite(errorPin, LOW);
 
-  //Setup PWM pin
+  // Setup PWM pin
   InitTimersSafe();
   bool success = SetPinFrequencySafe(pwmPin, 20000);
 
-  //Check if PWM pin was setup correctly
-  if (success) {
+  // Check if PWM pin was setup correctly
+  if (success)
+  {
     pwmWrite(pwmPin, 64);
   }
-  else { //Set errorPin HIGH if it was not
-
-    digitalWrite(10, HIGH);
+  else
+  { // Blink errorPin HIGH if it was not
+    while (true)
+    {
+      digitalWrite(10, HIGH);
+      delay(500);
+      digitalWrite(10, LOW);
+      delay(500);
+    }
   }
 }
 
-void loop() {
-
+void loop()
+{
 }
